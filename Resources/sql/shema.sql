c18fef6ddeb7b50ed038726c5af72b5fcc7a7497
Morel Sébastien <morel.seb@gmail.com>
2015-02-05 10:45:51 -0800

    Content in TEXT because often longer than 255

--- Resources/sql/shema.sql
+++ Resources/sql/shema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `novaseo_meta` (
   `objectattribute_id` bigint(20) unsigned NOT NULL,
   `meta_name` varchar(255) NOT NULL,
-  `meta_content` varchar(255) NOT NULL,
+  `meta_content` text NOT NULL,
   `objectattribute_version` int(10) unsigned NOT NULL,
   PRIMARY KEY (`objectattribute_id`,`objectattribute_version`,`meta_name`),
   KEY `novaseo_idx_content` (`objectattribute_id`,`objectattribute_version`) USING BTREE
