2f5d4008988f3a58b69398375cb1325bd5e21e64
Morel Sébastien <morel.seb@gmail.com>
2015-02-24 00:13:29 -0800

    config travis

--- build/.travis/bundle_setup.sh
+++ build/.travis/bundle_setup.sh
@@ -13,6 +13,9 @@
 # This script helps you setup your CI environment to run tests
 #################################################################
 
+echo "> Install bundle dependencies"
+composer require novactive/phpcs-novastandards:~1.3 phpmd/phpmd:~2.1 sebastian/phpcpd:~2.0 phpunit/phpunit:~4.4
+
 echo "> Enable bundle"
 sed -i.bak 's#new EzPublishLegacyBundle(),#new EzPublishLegacyBundle(),\n            new Novactive\Bundle\eZSEOBundle\NovaeZSEOBundle(),#g' ${TRAVIS_BUILD_DIR}/ezpublish/EzPublishKernel.php
 
@@ -29,6 +32,7 @@ php ezpublish/console ezpublish:legacybundles:install_extensions
 cd ${TRAVIS_BUILD_DIR}/ezpublish_legacy
 php bin/php/ezpgenerateautoloads.php -e
 cd ${TRAVIS_BUILD_DIR}
+php ezpublish/console clear:cache
 
 echo "> Create bundle table"
 mysql -u root behattestdb < ${TRAVIS_BUILD_DIR}/${NOVABUNDLE_PATH}/Resources/sql/shema.sql
