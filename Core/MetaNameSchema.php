fd014c2ce723c48a66c351afe27e668944927463
ychakroun <y.chakroun@novactive.com>
2016-04-28 04:28:18 -0400

    Add variation to fix #21
    (cherry picked from commit a64ecb7)

--- Core/MetaNameSchema.php
+++ Core/MetaNameSchema.php
@@ -12,7 +12,7 @@ namespace Novactive\Bundle\eZSEOBundle\Core;
 
 use eZ\Publish\API\Repository\Values\Content\Field;
 use eZ\Publish\Core\Repository\Values\Content\VersionInfo;
-use eZ\Publish\Core\Repository\NameSchemaService;
+use eZ\Publish\Core\Repository\Helper\NameSchemaService;
 use eZ\Publish\API\Repository\Values\Content\Content;
 use eZ\Publish\API\Repository\Values\ContentType\ContentType;
 use eZ\Publish\Core\FieldType\XmlText\Converter\Html5 as Html5Converter;
@@ -20,6 +20,13 @@ use eZ\Publish\SPI\Variation\VariationHandler;
 use eZ\Publish\Core\FieldType\XmlText\Value as XmlTextValue;
 use eZ\Publish\Core\FieldType\Relation\Value as RelationValue;
 use eZ\Publish\Core\FieldType\Image\Value as ImageValue;
+use eZ\Publish\SPI\Persistence\Content\Type as SPIContentType;
+use eZ\Publish\Core\Base\Exceptions\InvalidArgumentType;
+use eZ\Publish\SPI\Persistence\Content\Type\Handler as ContentTypeHandler;
+use eZ\Publish\Core\Base\Container\ApiLoader\FieldTypeCollectionFactory;
+use eZ\Publish\Core\Repository\Helper\FieldTypeRegistry;
+use eZ\Publish\API\Repository\Repository as RepositoryInterface;
+use eZ\Publish\Core\Helper\TranslationHelper;
 
 /**
  * Class MetaNameSchema
@@ -48,6 +55,20 @@ class MetaNameSchema extends NameSchemaService
     protected $imageVariationService;
 
     /**
+     * The repository
+     *
+     * @var RepositoryInterface
+     */
+    protected $repository;
+
+    /**
+     * Translation Helper
+     *
+     * @var TranslationHelper
+     */
+    protected $translationHelper;
+
+    /**
      * Meta content data max length
      *
      * @var int
@@ -55,11 +76,35 @@ class MetaNameSchema extends NameSchemaService
     protected $fieldContentMaxLength = 255;
 
     /**
+     * MetaNameSchema constructor.
+     *
+     * @param ContentTypeHandler         $contentTypeHandler
+     * @param FieldTypeCollectionFactory $collectionFactory
+     * @param RepositoryInterface        $repository
+     * @param TranslationHelper          $helper
+     * @param array                      $settings
+     */
+    public function __construct(
+        ContentTypeHandler $contentTypeHandler,
+        FieldTypeCollectionFactory $collectionFactory,
+        RepositoryInterface $repository,
+        TranslationHelper $helper,
+        array $settings
+    ) {
+        $fieldTypes        = $collectionFactory->getFieldTypes();
+        $registry          = new FieldTypeRegistry($fieldTypes);
+        $settings['limit'] = $this->fieldContentMaxLength;
+        parent::__construct($contentTypeHandler, $registry, $settings);
+        $this->repository        = $repository;
+        $this->translationHelper = $helper;
+    }
+
+    /**
      * Set prioritized languages
      *
      * @param array $languages
      */
-    public function setLanguages( array $languages = null )
+    public function setLanguages(array $languages = null)
     {
         $this->languages = $languages;
     }
@@ -69,7 +114,7 @@ class MetaNameSchema extends NameSchemaService
      *
      * @param Html5Converter $converter
      */
-    public function setHtml5Converter( Html5Converter $converter )
+    public function setHtml5Converter(Html5Converter $converter)
     {
         $this->html5Converter = $converter;
     }
@@ -79,7 +124,7 @@ class MetaNameSchema extends NameSchemaService
      *
      * @param VariationHandler $handler
      */
-    public function setImageVariationService( VariationHandler $handler )
+    public function setImageVariationService(VariationHandler $handler)
     {
         $this->imageVariationService = $handler;
     }
@@ -93,16 +138,9 @@ class MetaNameSchema extends NameSchemaService
      *
      * @return boolean
      */
-    public function resolveMeta( Meta $meta, Content $content, ContentType $contentType = null )
+    public function resolveMeta(Meta $meta, Content $content, ContentType $contentType = null)
     {
-
-        $this->settings = array(
-            'limit' => $this->fieldContentMaxLength,
-            'sequence' => '...',
-        );
-
-        if ( $contentType === null )
-        {
+        if ($contentType === null) {
             $contentType = $this->repository->getContentTypeService()->loadContentType(
                 $content->contentInfo->contentTypeId
             );
@@ -115,35 +153,56 @@ class MetaNameSchema extends NameSchemaService
             $content->versionInfo->languageCodes
         );
         // we don't fallback on the other languages... it would be very bad for SEO to mix the languages
-        if ( ( array_key_exists( $this->languages[0], $resolveMultilingue ) ) &&
-             ( $resolveMultilingue[$this->languages[0]] != '' )
-        )
-        {
-            $meta->setContent( $resolveMultilingue[$this->languages[0]] );
+        if ((array_key_exists($this->languages[0], $resolveMultilingue)) &&
+            ($resolveMultilingue[$this->languages[0]] != '')
+        ) {
+            $meta->setContent($resolveMultilingue[$this->languages[0]]);
+
             return true;
         }
-        $meta->setContent( "" );
+        $meta->setContent("");
+
         return false;
     }
+
 
     /**
      * {@inheritdoc}
      */
-    protected function getFieldTitles( array $schemaIdentifiers, ContentType $contentType, array $fieldMap, $languageCode )
+    protected function getFieldTitles(array $schemaIdentifiers, $contentType, array $fieldMap, $languageCode)
     {
-        $fieldTitles = array();
+        $fieldTitles = array ();
 
-        foreach ( $schemaIdentifiers as $fieldDefinitionIdentifier )
-        {
-            if ( isset( $fieldMap[$fieldDefinitionIdentifier][$languageCode] ) )
-            {
-                $fieldDefinition = $contentType->getFieldDefinition( $fieldDefinitionIdentifier );
-                $fieldType       = $this->repository->getFieldTypeService()->getFieldType(
-                    $fieldDefinition->fieldTypeIdentifier
-                );
+        foreach ($schemaIdentifiers as $fieldDefinitionIdentifier) {
+            if (isset($fieldMap[$fieldDefinitionIdentifier][$languageCode])) {
+                if ($contentType instanceof SPIContentType) {
+                    $fieldDefinition = null;
+                    foreach ($contentType->fieldDefinitions as $spiFieldDefinition) {
+                        if ($spiFieldDefinition->identifier === $fieldDefinitionIdentifier) {
+                            $fieldDefinition = $spiFieldDefinition;
+                            break;
+                        }
+                    }
+
+                    if ($fieldDefinition === null) {
+                        $fieldTitles[$fieldDefinitionIdentifier] = '';
+                        continue;
+                    }
+
+                    $fieldType = $this->fieldTypeRegistry->getFieldType(
+                        $fieldDefinition->fieldType
+                    );
+                } elseif ($contentType instanceof ContentType) {
+                    $fieldDefinition = $contentType->getFieldDefinition($fieldDefinitionIdentifier);
+                    $fieldType       = $this->fieldTypeRegistry->getFieldType(
+                        $fieldDefinition->fieldTypeIdentifier
+                    );
+                } else {
+                    throw new InvalidArgumentType('$contentType', 'API or SPI variant of ContentType');
+                }
+
                 // eZ XML Text
-                if ( $fieldMap[$fieldDefinitionIdentifier][$languageCode] instanceof XmlTextValue )
-                {
+                if ($fieldMap[$fieldDefinitionIdentifier][$languageCode] instanceof XmlTextValue) {
                     $fieldTitles[$fieldDefinitionIdentifier] = $this->handleXmlTextValue(
                         $fieldMap[$fieldDefinitionIdentifier][$languageCode]
                     );
@@ -151,19 +210,20 @@ class MetaNameSchema extends NameSchemaService
                 }
 
                 //eZ Object Relation
-                if ( $fieldMap[$fieldDefinitionIdentifier][$languageCode] instanceof RelationValue )
-                {
+                if ($fieldMap[$fieldDefinitionIdentifier][$languageCode] instanceof RelationValue) {
                     $fieldTitles[$fieldDefinitionIdentifier] = $this->handleRelationValue(
-                        $fieldMap[$fieldDefinitionIdentifier][$languageCode], $languageCode
+                        $fieldMap[$fieldDefinitionIdentifier][$languageCode],
+                        $languageCode
                     );
                     continue;
                 }
 
                 // eZ Image
-                if ( $fieldMap[$fieldDefinitionIdentifier][$languageCode] instanceof ImageValue )
-                {
+                if ($fieldMap[$fieldDefinitionIdentifier][$languageCode] instanceof ImageValue) {
                     $fieldTitles[$fieldDefinitionIdentifier] = $this->handleImageValue(
-                        $fieldMap[$fieldDefinitionIdentifier][$languageCode], $fieldDefinitionIdentifier, $languageCode
+                        $fieldMap[$fieldDefinitionIdentifier][$languageCode],
+                        $fieldDefinitionIdentifier,
+                        $languageCode
                     );
                     continue;
                 }
@@ -173,6 +233,7 @@ class MetaNameSchema extends NameSchemaService
                 );
             }
         }
+
         return $fieldTitles;
     }
 
@@ -186,7 +247,7 @@ class MetaNameSchema extends NameSchemaService
      *
      * @return string
      */
-    protected function getVariation( $value, $identifier, $languageCode, $variationName )
+    protected function getVariation($value, $identifier, $languageCode, $variationName)
     {
         // @todo: I don't know how to do differently here...
         $field     = new Field(
@@ -196,7 +257,7 @@ class MetaNameSchema extends NameSchemaService
                 'languageCode'       => $languageCode
             ]
         );
-        $variation = $this->imageVariationService->getVariation( $field, new VersionInfo(), $variationName );
+        $variation = $this->imageVariationService->getVariation($field, new VersionInfo(), $variationName);
 
         return $variation->uri;
     }
@@ -208,9 +269,9 @@ class MetaNameSchema extends NameSchemaService
      *
      * @return string
      */
-    protected function handleXmlTextValue( XmlTextValue $value )
+    protected function handleXmlTextValue(XmlTextValue $value)
     {
-        return trim( strip_tags( $this->html5Converter->convert( $value->xml ) ) );
+        return trim(strip_tags($this->html5Converter->convert($value->xml)));
     }
 
     /**
@@ -221,22 +282,19 @@ class MetaNameSchema extends NameSchemaService
      *
      * @return string
      */
-    protected function handleRelationValue( RelationValue $value, $languageCode )
+    protected function handleRelationValue(RelationValue $value, $languageCode)
     {
-        if ( !$value->destinationContentId )
-        {
+        if (!$value->destinationContentId) {
             return '';
         }
-        $relatedContent = $this->repository->getContentService()->loadContent( $value->destinationContentId );
+        $relatedContent = $this->repository->getContentService()->loadContent($value->destinationContentId);
         // @todo: we can probably be better here and handle more than just "image"
-        if ( $fieldImageValue = $relatedContent->getFieldValue( 'image' ) )
-        {
-            if ( $fieldImageValue->uri )
-            {
-                return $this->getVariation( $fieldImageValue, "image", $languageCode, "medium" );
+        if ($fieldImageValue = $relatedContent->getFieldValue('image')) {
+            if ($fieldImageValue->uri) {
+                return $this->getVariation($fieldImageValue, "image", $languageCode, "social_network_image");
             }
         }
-        return '';
+        return $this->translationHelper->getTranslatedContentName($relatedContent, $languageCode);
     }
 
     /**
@@ -248,17 +306,17 @@ class MetaNameSchema extends NameSchemaService
      *
      * @return string
      */
-    protected function handleImageValue( ImageValue $value, $fieldDefinitionIdentifier, $languageCode )
+    protected function handleImageValue(ImageValue $value, $fieldDefinitionIdentifier, $languageCode)
     {
-        if ( !$value->uri )
-        {
+        if (!$value->uri) {
             return '';
         }
+
         return $this->getVariation(
             $value,
             $fieldDefinitionIdentifier,
             $languageCode,
-            "medium"
+            "social_network_image"
         );
     }
 }
