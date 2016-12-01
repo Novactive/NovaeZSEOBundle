dab22a715295f44aea492b800c59718ca92237ab
Florian ALEXANDRE <f.alexandre@novactive.com>
2016-12-01 12:16:24 +0100

    novaseometas field type edit view

--- Resources/public/js/views/fields/novaseometas-editview.js
+++ Resources/public/js/views/fields/novaseometas-editview.js
@@ -3,16 +3,44 @@ YUI.add('novaseometas-editview', function (Y) {
     Y.namespace('Novactive');
 
     var L = Y.Lang,
+        IS_VISIBLE_CLASS = 'is-visible',
+        IS_SHOWING_DESCRIPTION = 'is-showing-description',
+        FIELD_INPUT = '.ez-editfield-input',
+        TOOLTIP_DESCR = '.ez-field-description',
+        STANDARD_DESCR = 'ez-standard-description',
         FIELDTYPE_IDENTIFIER = 'novaseometas';
 
     Y.Novactive.NovaSEOMetasEditView = Y.Base.create('novaseometasEditView', Y.eZ.FieldEditView, [], {
+        /**
+         * Custom initializer method, it sets the event handling on the
+         * errorStatusChange event
+         *
+         * @method initializer
+         */
         initializer: function () {
             this._setMetasEditViews();
         },
 
         _setMetasEditViews: function () {
-            var views;
+            var views = [],
+                viewsById = [],
+                view,
+                fieldId = this.get('field').get('id'),
+                config = this.get('config');
+
+            Y.Array.each(config.seoMetas, function (meta) {
+                view = new Y.Novactive.MetaEditView({
+                    fieldId: fieldId,
+                    default_pattern: meta.default_pattern,
+                    icon: meta.icon,
+                    identifier: meta.identifier,
+                    label: meta.label
+                });
+                viewsById[meta.identifier] = view;
+                views.push(view);
+            });
             this._metaEditViews = views;
+            this._fieldEditViewsByDefinitionId = viewsById;
         },
 
         _renderFieldEditViews: function () {
@@ -26,9 +54,66 @@ YUI.add('novaseometas-editview', function (Y) {
             });
         },
 
+        /**
+         * Default implementation of the field edit view render. By default, it
+         * injects the field definition, the field, the content and the content
+         * type. If the view is already active, it calls `_afterActiveReRender`
+         * after the rendering process so that the field edit view
+         * implementation has a way to handle *rerender* for instance when
+         * switching language in the UI.
+         *
+         * @method render
+         * @return {eZ.FieldEditView}
+         */
         render: function () {
+            var def = this.get('fieldDefinition'),
+                defaultVariables = {
+                    fieldDefinition: def,
+                    field: this.get('field'),
+                    content: this.get('content').toJSON(),
+                    version: this.get('version').toJSON(),
+                    contentType: this.get('contentType').toJSON(),
+                },
+                container = this.get('container');
+
+            container.setAttribute('data-field-definition-identifier', def.identifier);
+
+            if (this._useStandardFieldDefinitionDescription) {
+                container.addClass(STANDARD_DESCR);
+            }
+            this.get('container').setHTML(
+                this.template(Y.mix(this._variables(), defaultVariables, true))
+            );
+
+            if (this._isTouch()) {
+                container.addClass('is-using-touch-device');
+            }
+
+            if (this.get('active')) {
+                this._afterActiveReRender();
+            }
+
             this._renderFieldEditViews();
-        }
+            return this;
+        },
+
+        /**
+         * Returns the value of the field from the current user input. This
+         * method should be implemented in each field edit view.
+         *
+         * The default implementation returns undefined. Returning undefined
+         * means that the field should be ignored when saving the content.
+         *
+         * @method _getFieldValue
+         * @protected
+         * @return mixed
+         */
+        _getFieldValue: function () {
+            console.error(
+                'Error: _getFieldValue() is not implemented in ' + this.constructor.NAME
+            );
+            return undefined;
+        },
     });
 
     Y.eZ.FieldEditView.registerFieldEditView(
