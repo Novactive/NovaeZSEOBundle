286f3771109c4f74eb10a926b951ca8e0dd5797c
f.alexandre <f.alexandre@novactive.com>
2016-12-07 12:09:30 +0100

    Field value rendering in plateformUI

--- Resources/public/js/views/value/meta-view.js
+++ Resources/public/js/views/value/meta-view.js
@@ -3,6 +3,24 @@ YUI.add('meta-view', function (Y) {
     Y.namespace('Novactive');
 
     Y.Novactive.MetaView = Y.Base.create('metaView', Y.eZ.TemplateBasedView, [], {
+        render: function () {
+            var value = this.get('value');
+            this.get('container').setHTML(this.template({
+                label: this.get('label'),
+                value: value != undefined && value.meta_content != "" ? value.meta_content : this.get("default_pattern")
+            }));
 
+            return this;
+        }
+    },
+    {
+        ATTRS: {
+            fieldId: null,
+            default_pattern: null,
+            icon: null,
+            identifier: null,
+            label: null,
+            value: null
+        }
     });
 });
