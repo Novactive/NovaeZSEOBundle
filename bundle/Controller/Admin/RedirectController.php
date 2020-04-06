f54ed5fede56c7a031d0059c84244b39d16898e4
Jani Tarvainen <janit@iki.fi>
2020-04-06 11:04:25 +0300

    Work on v3.0.0 final compatibility

--- bundle/Controller/Admin/RedirectController.php
+++ bundle/Controller/Admin/RedirectController.php
@@ -32,7 +32,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @Route("/novaseo/redirect")
