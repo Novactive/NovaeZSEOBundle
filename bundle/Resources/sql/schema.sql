caebe627263a1ae4d1ed4b2d2cceb1df5f030ce1
Florian ALEXANDRE <f.alexandre@novactive.com>
2023-06-16 15:51:21 +0200

    fix: switch CI env to prod

--- bundle/Resources/sql/schema.sql
+++ bundle/Resources/sql/schema.sql
@@ -7,10 +7,3 @@ CREATE TABLE `novaseo_meta` (
   KEY `novaseo_idx_content` (`objectattribute_id`,`objectattribute_version`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `novaseo_redirect_import_history` (
-	`id` INT AUTO_INCREMENT NOT NULL,
-	`name_file` VARCHAR (255) NOT NULL,
-	`date` DATETIME NOT NULL,
-	`path` VARCHAR (255) NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
