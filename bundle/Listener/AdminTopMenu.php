605702d9ccf9222d0eaa3f1d7dc5cdcca9c269e9
Lhassan BOUHOU <lhassan.bouhou@almaviacx.com>
2025-02-17 10:35:39 +0100

    Fix access display menu

--- bundle/Listener/AdminTopMenu.php
+++ bundle/Listener/AdminTopMenu.php
@@ -13,11 +13,19 @@
 namespace Novactive\Bundle\eZSEOBundle\Listener;
 
 use Ibexa\AdminUi\Menu\Event\ConfigureMenuEvent;
+use Ibexa\Contracts\Core\Repository\PermissionResolver;
 
 class AdminTopMenu
 {
+    public function __construct(protected PermissionResolver $permissionResolver)
+    {
+    }
+
     public function onMenuConfigure(ConfigureMenuEvent $event): void
     {
+        if (!$this->permissionResolver->hasAccess('novaseobundle.redirects', 'view')) {
+            return;
+        }
         $menu = $event->getMenu();
         $menu->addChild(
             'nova_create_redirect'
